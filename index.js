dc4eb08b138040803b2de656fa51eaebee7ea8e5
Damian Krzeminski <pirxpilot@code42day.com>
2017-05-17 17:37:22 -0700

    add dummy implementaion of database
    indexedDB in older browsers has severe limitations and bugs
    specifically storing binary data might not be supported
    testing for proper support might be tricky (async etc.) - so we let
    application decide how to do that
    
    if '_mlasq_old_browser' is set in global namespace (window) we are going
    to use dummy/empty implementation

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-module.exports = require('./lib/mlasq');
+var mlasq = require('./lib/mlasq');
+var dummy = require('./lib/dummy');
+
+/* global window */
+
+module.exports = detect() ? mlasq : dummy;
+
+function detect() {
+  return ('indexedDB' in window) && !('_mlasq_old_browser' in window);
+}
