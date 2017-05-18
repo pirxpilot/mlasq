ad1d3cce582538ef268e31116f92381786b4db35
Damian Krzeminski <pirxpilot@code42day.com>
2017-05-17 19:14:45 -0700

    use `self` instead of `window` for global context
    `self` works in web workers, `window` doesn't
    https://developer.mozilla.org/en-US/docs/Web/API/Window/self

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var mlasq = require('./lib/mlasq');
 var dummy = require('./lib/dummy');
 
-/* global window */
+/* global self */
 
 module.exports = detect() ? mlasq : dummy;
 
 function detect() {
-  return ('indexedDB' in window) && !('_mlasq_old_browser' in window);
+  return ('indexedDB' in self) && !('_mlasq_old_browser' in self);
 }
